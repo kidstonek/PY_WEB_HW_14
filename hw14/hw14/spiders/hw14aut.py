ca305ad548a206f04c541b52b9ef51f37232daf0
Andrii V <kidstonek@gmail.com>
2022-10-15 15:19:48 +0300

    clear something

--- hw14/hw14/spiders/hw14aut.py
+++ hw14/hw14/spiders/hw14aut.py
@@ -7,11 +7,7 @@ class Hw14autSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        # print(response.xpath("/html//span/a/@href").get())
         for quote in response.xpath("/html//div[@class='quote']"):
-            # q = quote.xpath("div[@class='author']/a/text()").extract()
-            # q = quote.xpath("/html//span/a/@href").get()
-            # print(q)
             yield {
                 "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
                 "author": quote.xpath("span/small/text()").extract(),
